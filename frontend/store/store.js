86834c18a7200613edf75bb4f0dbc32e5f4fac9e
Calvin McElroy <fourwallsstudio@gmail.com>
2017-07-19 07:00:47 -0400

    remove getState and logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
