bd8523a4e842cec25fe2429772c0ee2f522624c6
Calvin McElroy <fourwallsstudio@gmail.com>
2017-08-29 13:10:30 -0400

    prep heroku push

--- frontend/selector.jsx
+++ frontend/selector.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-require('newrelic');
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
