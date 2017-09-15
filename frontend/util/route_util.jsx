0a939568411ce29a5e199d40a83f20ec8dfdd73b
Calvin McElroy <fourwallsstudio@gmail.com>
2017-09-15 18:46:55 -0400

    refactor

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -14,7 +14,7 @@ const Auth = ({component: Component, path, loggedIn}) => (
 
 const Protected = ({ component: Component, path, loggedIn }) => (
   <Route path={path} render={(props) => (
-     loggedIn ? (
+    loggedIn ? (
       <Component {...props} />
     ) : (
       <Redirect to="/" />
