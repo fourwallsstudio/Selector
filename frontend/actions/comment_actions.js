f30f391164fce5bf106050a8798354e1ed594a4c
Calvin McElroy <fourwallsstudio@gmail.com>
2017-09-27 18:43:57 -0400

    comment actions

--- frontend/actions/comment_actions.js
+++ frontend/actions/comment_actions.js
@@ -7,6 +7,8 @@ export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
 export const CREATE_COMMENT = 'CREATE_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 
+
+
 export const createComment = comment => {
   return dispatch => {
     return APIUtil.createComment(comment)
