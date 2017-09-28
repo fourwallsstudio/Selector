acdb18cf7d3109770415e59aaa603c249fd955d5
Calvin McElroy <fourwallsstudio@gmail.com>
2017-09-27 20:09:02 -0400

    add filter headers

--- frontend/components/user_welcome/user_welcome.jsx
+++ frontend/components/user_welcome/user_welcome.jsx
@@ -4,16 +4,22 @@ import UserWelcomeAside from './user_welcome_aside';
 import ShowFeedContainer from '../show_feed/show_feed_container';
 
 const UserWelcome = props => {
+
+  let feedType;
+
+  if (props.filter === 'most_recent') feedType = ' - New Shows';
+  if (props.filter === 'trending') feedType = ' - Trending';
+
   return (
     <section className="user-welcome-container">
       <InnerHeader
         fetchAllShows={ props.fetchAllShows }
-        updateFilter={ props.updateFilter} />
+        updateFilter={ props.updateFilter } />
       <div className="inner-header-clear"></div>
 
       <div className="user-welcome-feed">
         <div className="user-welcome-feed-head">
-          <h2>Feed</h2>
+          <h2>Feed{ feedType }</h2>
         </div>
         <ShowFeedContainer
           filter={ props.filter } />
